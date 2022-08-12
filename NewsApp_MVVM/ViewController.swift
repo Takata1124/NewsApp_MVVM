71f324d6ca9278a993986a003758667ab4c2b8ca
Takata1124 <skkyosk1124@gmail.com>
2022-08-12 09:23:43 +0900

    Initial Commit

---
+++ NewsApp_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp_MVVM
+//
+//  Created by t032fj on 2022/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
